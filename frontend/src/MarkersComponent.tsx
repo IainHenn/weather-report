5c3933455feaf290807f44abe7080328e9614976
IainHenn <Iainevans2005@gmail.com>
2025-04-19 10:55:17 -0700

    Feature: Basic front end api request for city specific data

--- frontend/src/MarkersComponent.tsx
+++ frontend/src/MarkersComponent.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState} from 'react';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import {Marker, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LeafletMouseEvent } from 'leaflet';
 
 function MarkersComponent() {
     const [cities, setCities] = useState([]);
@@ -21,7 +22,9 @@ function MarkersComponent() {
                 {cities.map((city, index) => {
                     console.log(city);
                     return (
-                        <Marker key={city.id} position={[city.lat, city.lon]}>
+                        <Marker key={city.id} position={[city.lat, city.lon]} eventHandlers={{
+                            click: () => retrieveInfo(city.id),
+                        }}>
                             <Popup>
                                 <strong>{city.name}</strong>
                             </Popup>
@@ -31,6 +34,15 @@ function MarkersComponent() {
             </MarkerClusterGroup>
         </>
     )
+}
+
+function retrieveInfo(cityId: number) {
+    fetch(`http://localhost:8080/city?id=${cityId}`)
+        .then(resp => resp.json())
+        .then(city => {
+            console.log(`city: ${JSON.stringify(city)}`);
+        })
+        .catch(err => console.log(err))
 }
 
 export default MarkersComponent;
